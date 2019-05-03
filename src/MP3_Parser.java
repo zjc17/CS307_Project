761a40ea5b42cdbec8c35e78e2e9a6cfd1d35b98
Jiachen-Dell <acezjc@foxmail.com>
2019-05-03 17:33:21 +0800

    update

---
+++ src/MP3_Parser.java
@@ -0,0 +1,8 @@
+public class MP3_Parser {
+  public MP3_Parser(String path) {
+    if (!path.endsWith(".mp3")) {
+      throw new IllegalArgumentException("Not support this type of file.");
+    }
+    /*  */
+  }
+}
