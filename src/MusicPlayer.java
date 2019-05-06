70a29a6cc8c9261cc01bb0f1f6d1cf4fe8cadb5f
Jiachen-Dell <acezjc@foxmail.com>
2019-05-06 23:16:54 +0800

    accumulate the lib of javafx

---
+++ src/MusicPlayer.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MusicPlayer extends Application {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+
+  }
+}
